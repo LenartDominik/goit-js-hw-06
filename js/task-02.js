0f41d2f752c6fc4854b74acc88553150c747104a
Dominik Lenart <dominik.lenart84@gmail.com>
2022-12-12 15:32:00 +0100

    task 02

--- js/task-02.js
+++ js/task-02.js
@@ -6,3 +6,22 @@ const ingredients = [
   'Herbs',
   'Condiments',
 ];
+
+const makeList = (ingredients) => {
+  const ingredientsList = document.querySelector("#ingredients");
+  ingredients.forEach((el) => {
+    const listItem = document.createElement("li");
+    listItem.textContent = el;
+    ingredientsList.append(listItem);
+  });
+};
+
+makeList(ingredients);
+
+
+
+
+
+
+
+
