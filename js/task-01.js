564a593387163b4f7e70ba0d68638bc87b028543
Dominik Lenart <dominik.lenart84@gmail.com>
2022-12-12 13:16:05 +0100

    task1

--- js/task-01.js
+++ js/task-01.js
@@ -3,10 +3,8 @@ const categoriesList = document.querySelector("ul#categories");
 const numCategories = categoriesList.childElementCount;
 
 for (let i = 0; i < numCategories; i++) {
-
-  const categoryHeader = categoriesList.children[i].querySelector('h2');
-    const numItems = categoriesList.children[i].querySelectorAll('li').length;
-    
+  const categoryHeader = categoriesList.children[i].querySelector("h2");
+  const numItems = categoriesList.children[i].querySelectorAll("li").length;
+  
   console.log(`Category: ${categoryHeader.textContent} (${numItems} items)`);
 }
-
