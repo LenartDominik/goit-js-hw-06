9f59af9b4b1272887e176c89b60cdcba229744cb
Dominik Lenart <dominik.lenart84@gmail.com>
2022-12-19 17:38:51 +0100

    task 9

--- js/task-09.js
+++ js/task-09.js
@@ -1,3 +1,14 @@
+const changeColor = document.querySelector(".change-color");
+const hexColor = document.querySelector(".color");
+
+changeColor.addEventListener("click", () => {
+  const randomColor = getRandomHexColor();
+  document.body.style.backgroundColor = randomColor;
+  hexColor.textContent = randomColor;
+});
+
 function getRandomHexColor() {
-  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
-}
+  return `#${Math.floor(Math.random() * 16777215)
+    .toString(16)
+    .padStart(6, 0)}`;
+};
