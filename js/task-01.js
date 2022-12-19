6da6a4a49f781dcc9c87c1fc1cc3dcb8126fad7f
Dominik Lenart <dominik.lenart84@gmail.com>
2022-12-19 18:02:31 +0100

    update task 1

--- js/task-01.js
+++ js/task-01.js
@@ -5,7 +5,6 @@ console.log(`Number of categories: ${liItems.length}`);
 liItems.forEach((e) =>
   console.log(
     `Category: ${e.querySelector("h2").textContent}\nElements: ${
-      e.querySelectorAll("li").length
-    } `
+      e.querySelectorAll("li").length} `
   )
 );
